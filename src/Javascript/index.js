091299c802a72c3a9968287b4da82b227404e56f
Danna <gutierrezdanna119@gmail.com>
2023-07-03 15:52:04 -0600

    avances 03-07-2023

---
+++ src/Javascript/index.js
@@ -0,0 +1,85 @@
+//Variables.
+
+let InformacionTarea = window.document.querySelector("#InformacionTarea");
+let ul = window.document.querySelector("ul");
+let empty = window.document.querySelector(".empty");
+let contador = window.document.querySelector("#contador");
+
+//Evento del botón agregar
+function add(e) {
+  e.preventDefault();
+
+  let tarea = InformacionTarea.value;
+
+  if (tarea !== "") {
+    let li = document.createElement("li");
+    let p = document.createElement("p");
+
+    p.textContent = tarea;
+
+    li.appendChild(p);
+    li.appendChild(btnCheck());
+    li.appendChild(AddDeleteBoton());
+    ul.appendChild(li);
+
+    InformacionTarea.value = "";
+    empty.style.display = "none";
+  } else {
+    alert("Ingresar texto");
+  }
+}
+
+function btnCheck() {
+  let Checkbox = document.createElement("input");
+  Checkbox.setAttribute("type", "checkbox");
+
+  Checkbox.addEventListener("click", function () {
+    if (Checkbox.checked) {
+      aumentarContador();
+    } else {
+      disminuirContador();
+    }
+  });
+
+  return Checkbox;
+}
+
+//Botón de eliminar.
+
+function AddDeleteBoton() {
+  let DeleteBtn = document.createElement("button");
+
+  DeleteBtn.textContent = "🦄";
+  DeleteBtn.className = "del";
+
+  DeleteBtn.addEventListener("click", (e) => {
+    let item = e.target.parentElement;
+
+    let checkbox = item.querySelector("input");
+
+    if (checkbox.checked) {
+      disminuirContador();
+    }
+    ul.removeChild(item);
+
+    let items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      empty.style.display = "block";
+    }
+  });
+  return DeleteBtn;
+}
+
+function disminuirContador() {
+  let cuenta = Number(contador.textContent);
+  cuenta = cuenta - 1;
+  contador.textContent = cuenta;
+}
+function aumentarContador() {
+  let cuenta = Number(contador.textContent);
+  cuenta = cuenta + 1;
+  contador.textContent = cuenta;
+}
+
+export { add };
---
+++ src/Javascript/module.js
@@ -0,0 +1,5 @@
+import { add } from "./index.js";
+
+//Botón de agregar
+let Agregar = window.document.querySelector("#Agregar");
+Agregar.addEventListener("click", add);
